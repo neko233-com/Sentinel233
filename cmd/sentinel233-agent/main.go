168866574dc946fa5342f5b2901e533492435089
luohaojun <20020229+SolarisNeko@users.noreply.github.com>
2026-06-12 23:59:17 +0800

    fix: satisfy staticcheck for agent metrics

--- cmd/sentinel233-agent/main.go
+++ cmd/sentinel233-agent/main.go
@@ -63,39 +63,39 @@ func handleMetrics(w http.ResponseWriter, r *http.Request) {
 	var sb strings.Builder
 	sb.WriteString("# HELP process_cpu_seconds_total Total user and system CPU time spent in seconds.\n")
 	sb.WriteString("# TYPE process_cpu_seconds_total counter\n")
-	sb.WriteString(fmt.Sprintf("process_cpu_seconds_total %f\n", time.Since(startTime).Seconds()))
+	fmt.Fprintf(&sb, "process_cpu_seconds_total %f\n", time.Since(startTime).Seconds())
 
 	sb.WriteString("# HELP process_resident_memory_bytes Resident memory size in bytes.\n")
 	sb.WriteString("# TYPE process_resident_memory_bytes gauge\n")
-	sb.WriteString(fmt.Sprintf("process_resident_memory_bytes %d\n", m.Sys))
+	fmt.Fprintf(&sb, "process_resident_memory_bytes %d\n", m.Sys)
 
 	sb.WriteString("# HELP process_virtual_memory_bytes Virtual memory size in bytes.\n")
 	sb.WriteString("# TYPE process_virtual_memory_bytes gauge\n")
-	sb.WriteString(fmt.Sprintf("process_virtual_memory_bytes %d\n", m.Sys))
+	fmt.Fprintf(&sb, "process_virtual_memory_bytes %d\n", m.Sys)
 
 	sb.WriteString("# HELP go_goroutines Number of goroutines that currently exist.\n")
 	sb.WriteString("# TYPE go_goroutines gauge\n")
-	sb.WriteString(fmt.Sprintf("go_goroutines %d\n", runtime.NumGoroutine()))
+	fmt.Fprintf(&sb, "go_goroutines %d\n", runtime.NumGoroutine())
 
 	sb.WriteString("# HELP go_memstats_alloc_bytes Number of bytes allocated and still in use.\n")
 	sb.WriteString("# TYPE go_memstats_alloc_bytes gauge\n")
-	sb.WriteString(fmt.Sprintf("go_memstats_alloc_bytes %d\n", m.Alloc))
+	fmt.Fprintf(&sb, "go_memstats_alloc_bytes %d\n", m.Alloc)
 
 	sb.WriteString("# HELP go_memstats_total_alloc_bytes Total number of bytes allocated, even if freed.\n")
 	sb.WriteString("# TYPE go_memstats_total_alloc_bytes counter\n")
-	sb.WriteString(fmt.Sprintf("go_memstats_total_alloc_bytes %d\n", m.TotalAlloc))
+	fmt.Fprintf(&sb, "go_memstats_total_alloc_bytes %d\n", m.TotalAlloc)
 
 	sb.WriteString("# HELP go_memstats_sys_bytes Number of bytes obtained from system.\n")
 	sb.WriteString("# TYPE go_memstats_sys_bytes gauge\n")
-	sb.WriteString(fmt.Sprintf("go_memstats_sys_bytes %d\n", m.Sys))
+	fmt.Fprintf(&sb, "go_memstats_sys_bytes %d\n", m.Sys)
 
 	sb.WriteString("# HELP go_memstats_heap_alloc_bytes Number of heap bytes allocated and still in use.\n")
 	sb.WriteString("# TYPE go_memstats_heap_alloc_bytes gauge\n")
-	sb.WriteString(fmt.Sprintf("go_memstats_heap_alloc_bytes %d\n", m.HeapAlloc))
+	fmt.Fprintf(&sb, "go_memstats_heap_alloc_bytes %d\n", m.HeapAlloc)
 
 	sb.WriteString("# HELP go_info Go version info.\n")
 	sb.WriteString("# TYPE go_info gauge\n")
-	sb.WriteString(fmt.Sprintf("go_info{version=\"%s\"} 1\n", runtime.Version()))
+	fmt.Fprintf(&sb, "go_info{version=\"%s\"} 1\n", runtime.Version())
 
 	sb.WriteString("# HELP up 1 if the target is up, 0 if down.\n")
 	sb.WriteString("# TYPE up gauge\n")
